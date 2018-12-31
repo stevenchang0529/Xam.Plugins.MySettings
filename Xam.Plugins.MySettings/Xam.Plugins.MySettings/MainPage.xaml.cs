95c9a0f30651549b1e2ef6bdc5ab407461d2a1a9
Steven <steven@MSI-Steven>
2018-12-31 21:58:32 +0800

    加入專案檔案。

---
+++ Xam.Plugins.MySettings/Xam.Plugins.MySettings/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Xam.Plugins.MySettings
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.label.Text= Settings.MyValue??"Null";
+            if (Settings.MyValue == null)
+                Settings.MyValue = "Test Value";
+        }
+    }
+}
---
+++ Xam.Plugins.MySettings/Xam.Plugins.MySettings/Settings.cs
@@ -0,0 +1,13 @@
+﻿using Plugin.Settings;
+
+namespace Xam.Plugins.MySettings
+{
+    public static class Settings
+    {
+        public static string MyValue
+        {
+            get { return CrossSettings.Current.GetValueOrDefault("MyValue", null); }
+            set { CrossSettings.Current.AddOrUpdateValue("MyValue", value);  }
+        }
+    }
+}
